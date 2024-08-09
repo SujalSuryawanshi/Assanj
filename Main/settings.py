ac5ff63184aedb876ca53d409f83ed1dbba01867
Sujal Suryawanshi <[email protected]>
2024-08-09 16:13:11 +0530

    site.id

--- Main/settings.py
+++ Main/settings.py
@@ -39,7 +39,7 @@ AUTHENTICATION_BACKENDS = (
  'django.contrib.auth.backends.ModelBackend',
  'allauth.account.auth_backends.AuthenticationBackend',
  )
-SITE_ID = 10
+SITE_ID = 11
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL='/'
