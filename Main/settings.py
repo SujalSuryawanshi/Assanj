1c1d458581c185c4bdc24a8aa653a0ba74509385
Sujalsurya <105475892+SujalSuryawanshi@users.noreply.github.com>
2024-08-11 10:18:42 +0530

    Update settings.py

--- Main/settings.py
+++ Main/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'whitenoise.runserver_nonstatic',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'allauth',   
