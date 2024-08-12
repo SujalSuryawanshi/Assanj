0e89a5cc2e8e90c9376f8621099b52dabec3c7c7
Sujalsurya <105475892+SujalSuryawanshi@users.noreply.github.com>
2024-08-12 16:07:42 +0530

    Update urls.py

--- Main/urls.py
+++ Main/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from Main import settings
 from django.conf.urls import handler404
 
-handler404 = 'yourapp.views.custom_404'
+handler404 = 'core.views.custom_404'
 
 urlpatterns = [
     path("", include('core.urls')),
