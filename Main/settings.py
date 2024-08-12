8bedbbb1d3dcf9568301b47183918e59dfa2425b
Sujalsurya <105475892+SujalSuryawanshi@users.noreply.github.com>
2024-08-12 15:52:24 +0530

    Update settings.py

--- Main/settings.py
+++ Main/settings.py
@@ -6,11 +6,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-#ui%=r7zuufjfwktbv(#-1)%zgryp!2h*^^u&-q0x9pjpz(jyw'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.5:8000','*','assanj.in']
+ALLOWED_HOSTS = ['assanj.in','www.assanj.in','16.171.226.0']
 
-
+CSRF_COOKIE_SECURE=True
 
 # Application definition
 INSTALLED_APPS = [
