d209354c65bb07a45fcd33ed8123ee04d87372dc
Sujalsurya <105475892+SujalSuryawanshi@users.noreply.github.com>
2024-08-16 20:24:07 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -4,16 +4,21 @@ from django.db.models.signals import post_save
 ####(((CUSTOM-USER)))####
 ####((FRIENDS))####
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
+    def create_user(self, username,email, password=None, **extra_fields):
         if not username:
             raise ValueError('The Username field must be set')
+        if not email:
+            raise ValueError('The Email field must be set')
+        
+        email = self.normalize_email(email)
         user = self.model(username=username, **extra_fields)
+
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None, **extra_fields):
+    def create_superuser(self, username,email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -22,22 +27,22 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(username, password, **extra_fields)
+        return self.create_user(username,email, password, **extra_fields)
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(unique=True, max_length=30)
+    email = models.EmailField(unique=True, default='notset@gmail.com')
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     current_session_key=models.CharField(max_length=40, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    phone=models.CharField(max_length=15, unique=True, default='9730730614')
     friends = models.ManyToManyField('self', symmetrical=True, blank=True)
 
     objects = CustomUserManager()
     
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['email']
     
     def __str__(self):
         return self.username
